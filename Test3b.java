b0c6ebc2b5343eebab7a489f9821c888018630b6
dhanalakshmi900 <131343621+dhanalakshmi900@users.noreply.github.com>
2023-06-04 15:51:53 +0530

    Add files via upload

---
+++ Test3b.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Test3b{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+char sp=" ";
+for(int i=0;i<=10;i++)
+{
+System.out.print(num);
+System.out.println(sp+"*"+sp+i+ "="+(num*i));
+}
+}
+}
+
+
+
---
+++ TestStudent4.java
@@ -0,0 +1,23 @@
+class Student{
+int rollno;
+String name;
+void insertRecord(int r,String n){
+rollno=r;
+name=n;
+}
+void displayinformation(){
+System.out.println(rollno+" "+name);}
+}
+
+class TestStudent4{
+public static void main(String args[]){
+Student s1=new Student();
+Student s2=new Student();
+s1.insertRecord(111,"Dhana");
+s2.insertRecord(222,"Lakshmi");
+s1.displayinformation();
+s2.displayinformation();
+}
+}
+
+
